fff03f30395d949d2892d54dbb1f7daa2429194a
SomeGuyNamedMy <mfdear444@gmail.com>
2023-03-29 00:35:11 -0400

    fix

--- users/may/desktop.nix
+++ users/may/desktop.nix
@@ -135,6 +135,18 @@
   xdg.mime.enable = true;
   xdg.mimeApps = {
     enable = true;
+    associations.added = {
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+        "x-scheme-handler/chrome" = "firefox.desktop";
+        "text/html" = "firefox.desktop";
+        "application/x-extension-htm" = "firefox.desktop";
+        "application/x-extension-html" = "firefox.desktop";
+        "application/x-extension-shtml" = "firefox.desktop";
+        "application/xhtml+xml" = "firefox.desktop";
+        "application/x-extension-xhtml" = "firefox.desktop";
+        "application/x-extension-xht" = "firefox.desktop";
+    };
     defaultApplications = {
       "image/png" = [ "imv.desktop" ];
       "image/jpeg" = [ "imv.desktop" ];
@@ -142,6 +154,16 @@
       "application/pdf" = [ "org.pwmt.zathura-pdf-mupdf.desktop" ];
       "text/plain" = [ "emacs.desktop" ];
       "inode/directory" = [ "thunar.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/chrome" = [ "firefox.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+      "application/x-extension-htm" = [ "firefox.desktop" ];
+      "application/x-extension-html" = ["firefox.desktop"];
+      "application/x-extension-shtml" = ["firefox.desktop"];
+      "application/xhtml+xml" = ["firefox.desktop"];
+      "application/x-extension-xhtml" = ["firefox.desktop"];
+      "application/x-extension-xht" = ["firefox.desktop"];
     };
   };
 
